f7240f40fd3f3f1ad50de122508320146fd27de5
MounirBlk <mounir.ballouk@gmail.com>
2021-02-06 23:28:56 +0100

    test unit success v4

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -1,8 +1,9 @@
 const request = require('supertest');
 
-describe('Login test by rest API', async() => {
+describe('Login test by rest API', () => {
+
     // test permettant de tester si les variables sont bien rempli ou non
-    it('Login fail datas manquantes', async(done) => {
+    it('Login fail datas manquantes', (done) => {
         request('https://api-develop-ci.herokuapp.com')
             .post('/register')
             .send('')
