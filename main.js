06e4779f1ec01f302161f6aef13dc513d9902b7e
MounirBlk <57747445+MounirBlk@users.noreply.github.com>
2021-02-06 21:12:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ const express = require('express'),
     axios = require('axios').default,
     CronJob = require('cron').CronJob;
 
-// parse application/x-www-form-urlencoded
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type");
