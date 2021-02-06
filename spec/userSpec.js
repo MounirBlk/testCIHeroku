3664fc3b9d901ad15f547bc8974f42c192bbf0c1
MounirBlk <mounir.ballouk@gmail.com>
2021-02-06 23:13:02 +0100

    test unit success v2

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -3,26 +3,15 @@ const request = require('supertest');
 describe('Login test by rest API', () => {
 
     // test permettant de tester si les variables sont bien rempli ou non
-    it('Login fail email/password undefined', (done) => {
+    it('Login fail datas manquantes', (done) => {
         request('https://api-develop-ci.herokuapp.com')
-            .post('/login')
-            .send('email=&password=')
+            .post('/register')
+            .send('')
             .set('Accept', 'application/json')
             .expect(403, {
                 error: true,
-                message: "L'email/password est manquant"
+                message: "L'une ou plusieurs données obligatoire sont manquantes"
             }, done);
     });
 
-    // test permettant de tester si les variables sont bien conforme ou non
-    it('Login fail bad email', (done) => {
-        request('https://api-develop-ci.herokuapp.com')
-            .post('/login')
-            .send('email=unknown&password=unknown')
-            .set('Accept', 'application/json')
-            .expect(409, {
-                error: true,
-                message: "L'email n'est pas conforme"
-            }, done);
-    });
 })
