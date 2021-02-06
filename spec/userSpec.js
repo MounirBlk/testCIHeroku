356044e264488c4e60aed32cf090ff6385e8be21
MounirBlk <mounir.ballouk@gmail.com>
2021-02-07 00:26:38 +0100

    test

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -7,7 +7,7 @@ describe('Login test by rest API', () => {
             .post('/register')
             .send('')
             .set('Accept', 'application/json')
-            .expect(403, {
+            .expect(404, {
                 error: true,
                 message: "L'une ou plusieurs données obligatoire sont manquantes"
             }, done);
