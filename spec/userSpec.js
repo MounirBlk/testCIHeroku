9a4d676a1f2854b15c3314ac1324f659d879c617
MounirBlk <mounir.ballouk@gmail.com>
2021-02-21 16:12:22 +0100

    resolve

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 describe('Login test by rest API', () => {
     // test permettant de tester si les variables sont bien rempli ou non
     it('Login fail datas manquantes', (done) => {
-        request('https://localhost:3000') // https://api-develop-ci.herokuapp.com  https://localhost:3000
+        request('http://localhost:3000') // https://api-develop-ci.herokuapp.com  https://localhost:3000
             .post('/register')
             .send('')
             .set('Accept', 'application/json')
